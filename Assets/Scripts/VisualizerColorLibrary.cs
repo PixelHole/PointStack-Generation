af39bae8d2881cf6851bcf3621e46e02862b7ad9
PixelHole <66640043+PixelHole@users.noreply.github.com>
2022-09-11 22:15:57 +0430

    added height based color library

---
+++ Assets/Scripts/VisualizerColorLibrary.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class VisualizerColorLibrary : ScriptableObject
+{
+    private List<Vector2> heights = new List<Vector2>();
+    private List<Color> colors = new List<Color>();
+
+    public Color GetColorForHeight(float height)
+    {
+        Color heightColor = Color.magenta;
+        for (int i = 0; i < heights.Count; i++)
+        {
+            if (height > heights[i].x & height < heights[i].y)
+            {
+                heightColor = colors[i];
+                break;
+            }
+        }
+        return heightColor;
+    }
+}
